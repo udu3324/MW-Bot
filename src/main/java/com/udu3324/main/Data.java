6cd1fe67c7aa5a16ef81d8dfae0b22a62d5b8dc1
udu3324 <47045986+udu3324@users.noreply.github.com>
2022-02-17 18:30:26 -0600

    Change dir

--- src/main/java/com/udu3324/main/Data.java
+++ src/main/java/com/udu3324/main/Data.java
@@ -34,7 +34,7 @@ public class Data {
     //C:/Users/build/Desktop/MultiMC/instances/1.16.51/.minecraft/logs/latest.log
     //C:/Users/build/Desktop/MultiMC/instances/1.17.1/.minecraft/logs/latest.log
     //latest.log if file is in directory
-    public static final String logFile = "C:\\Users\\build\\Desktop\\MW-Bot\\latest.log";
+    public static final String logFile = "latest.log";
     public static final String activity = "play.minewind.net";
     public static final String command = ">";
     public static final String staffRoleID = "863977201324785685";
